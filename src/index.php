d2f691061510449b95e2c7ab75d0310ed9ebe188
Brent Barker <brentbarker9@gmail.com>
2015-10-08 23:40:17 -0600

    Finished up deliv 1

--- src/index.php
+++ src/index.php
@@ -11,46 +11,39 @@ $app = new Application();
 
 $app['debug'] = true;
 
-$inventory = [
-  '322323' => [
-    'name' => 'Bubbleicous',
-    'desc' => 'A yummy bubble gum',
-    'rank' => 'high'
-  ],
-  '83573897539' => [
-    'name' => 'Blow Pop',
-    'desc' => 'Tasty bubble gum inside a lolipop',
-    'rank' => 'super high'
-  ]
-];
-
-
 $notes = [
   '1' => [
     'name' => 'Sprinkler',
     'body' => 'Replace Sprinkler Body',
-    'tags' => ['Outside', 'Yard Work', 'Summer']
+    'tags' => 'Outside, Yard Work, Summer'
   ],
   '2' => [
     'name' => 'Brakes',
     'body' => 'Fix the brakes on truck',
-    'tags' => ['Outside', 'Vehicle']
+    'tags' => 'Outside, Vehicle'
   ]
 ];
 
+$users = [
+  '1' => [
+    'username' => 'foo',
+    'password' => 'private',
+    'firstname' => 'Brent',
+    'lastname' => 'Barker'
+  ],
+  '2' => [
+    'username' => 'bar',
+    'password' => 'private',
+    'firstname' => 'B',
+    'lastname' => 'Barke'
+  ],
+];
+
 $app->get('/', function () {
-  return new Response('<h1>ReST API Candy Demo</h1>', 200);
+  return new Response('<h1>Proj 1 Deliverable 1</h1>', 200);
 });
 
-$app->get('/barrell', function () use ($inventory) {
-  $jsons = json_encode($inventory);
-  $response = new Response($jsons, 200);
-  $response->headers->set('Content-type', 'application/json');
-  $response->headers->set('Content-length', strlen($jsons));
-
-  return $response;
-});
-
+/* create */
 $app->post('/notes', function (Application $app, Request $request) use ($notes) {
 
   $contentTypeValid = in_array(
@@ -72,52 +65,33 @@ $app->post('/notes', function (Application $app, Request $request) use ($notes) 
   ];
 
   return new Response(
-    json_encode($notes),
+    json_encode(['result' => 'success', 'status' => 'created']),
     201,
     [
       'Content-Type' => 'application/json',
-      'Location' => 'http://localhost:8888/notes/' . $newId
+      'Location' => 'http://localhost:8080/notes/' . $newId
     ]
   );
 });
 
-$app->get('/notes', function (Application $app) use ($notes) {
-
-  return new Response(
-    json_encode($notes),
-    201,
-    ['content-type' => 'application/json']
-  );
-});
-
+/* read */
 $app->get('/notes/{id}', function (Application $app, Request $request, $id) use ($notes) {
 
 
   $singleNote = $notes[$id];
 
   if (!isset($singleNote)) {
-    $app->abort(404, 'Note with ID {$id} does not exist.');
+    $app->abort(404, 'Note with ID $id does not exist.');
   }
 
   return new Response(
-    json_encode($singleNote),
+    json_encode($notes[$id]),
     201,
     ['content-type' => 'application/json']
   );
 });
 
-
-$app->delete('/notes/{id}', function (Application $app, $id) use ($notes) {
-  if (!isset($notes[$id])) {
-    $app->abort(404, 'Note with ID {' . $id . '} does not exist.');
-  }
-
-  unset($notes{$id});
-
-  return new Response(null, 204);
-});
-
-
+/* update */
 $app->put('/notes/{id}', function (Application $app, Request $request, $id) use ($notes) {
 
   $contentTypeValid = in_array(
@@ -130,39 +104,141 @@ $app->put('/notes/{id}', function (Application $app, Request $request, $id) use 
   }
 
   $content = json_decode($request->getContent(), true);
-  $newId = uniqid();
 
-  $notes[$newId] = [
+  if(!isset($notes[$id])) {
+    $app->abort(404, 'Note with ID $id does not exist.');
+  }
+
+  $notes[$id] = [
     'name' => $content->name,
     'body' => $content->body,
     'tags' => $content->tags
   ];
 
   return new Response(
-    json_encode($notes),
-    201,
-    ['Location' => 'http://localhost:8888/notes/' . $newId]
+    json_encode(['result' => 'success', 'status' => 'updated']),
+    200,
+    [
+      'Content-Type' => 'application/json',
+      'Location' => 'http://localhost:8080/notes/' . $id
+    ]
   );
 });
 
-/*
-$app->update('/notes/{id}', function(Application $app, $id) use ($notes) {
+/* delete */
+$app->delete('/notes/{id}', function (Application $app, $id) use ($notes) {
+  if (!isset($notes[$id])) {
+    $app->abort(404, 'Note with ID $id does not exist.');
+  }
 
+  unset($notes{$id});
+
+  return new Response(null, 204);
 });
-*/
 
-//Not implemented
-$app->put('/notes/{id}', function (Application $app, $id) use ($notes) {
-  return new Response(null, 501);
+/* list */
+$app->get('/notes', function (Application $app) use ($notes) {
+
+  return new Response(
+    json_encode($notes),
+    201,
+    ['content-type' => 'application/json']
+  );
+});
+
+/**users**/
+
+/* create */
+$app->post('/user', function (Application $app, Request $request) use ($users) {
+
+  $contentTypeValid = in_array(
+    'application/json',
+    $request->getAcceptableContentTypes()
+  );
+
+  if (!$contentTypeValid) {
+    $app->abort(406, 'Client must accept content type of "application/json"');
+  }
+
+  $content = json_decode($request->getContent());
+  $newId = uniqid();
+
+  $users[$newId] = [
+    'username' => $content->username,
+    'password' => $content->password,
+    'firstname' => $content->firstname,
+    'lastname' => $content->lastname
+  ];
+
+  return new Response(
+    json_encode(['result' => 'success', 'status' => 'created']),
+    201,
+    [
+      'Content-Type' => 'application/json',
+      'Location' => 'http://localhost:8080/user/' . $newId
+    ]
+  );
+});
+
+/*update*/
+$app->put('/user/{id}', function (Application $app, Request $request, $id) use ($users) {
+
+  $contentTypeValid = in_array(
+    'application/json',
+    $request->getAcceptableContentTypes()
+  );
+
+  if (!$contentTypeValid) {
+    $app->abort(406, 'Client must accept content type of "application/json"');
+  }
+
+  $content = json_decode($request->getContent());
+
+  if(!isset($users[$id])) {
+    $app->abort(404, 'User with ID $id does not exist.');
+  }
+
+  $users[$id] = [
+    'username' => $content->username,
+    'password' => $content->password,
+    'firstname' => $content->firstname,
+    'lastname' => $content->lastname
+  ];
+
+  return new Response(
+    json_encode(['result' => 'success', 'status' => 'updated']),
+    200,
+    ['Content-Type' => 'application/json']
+  );
+});
+
+
+/*delete*/
+$app->delete('/user/{id}', function (Application $app, $id) use ($users) {
+  if (!isset($users[$id])) {
+    $app->abort(404, 'User with ID $id does not exist.');
+  }
+
+  unset($users{$id});
+
+  return new Response(null, 204);
+});
+
+/*read*/
+$app->get('/user/{id}', function (Application $app, $id) use ($users) {
+  if (!isset($users[$id])) {
+    $app->abort(404, 'User with ID $id does not exist.');
+  }
+
+  return new Response(
+    json_encode($users[$id]),
+    200,
+    ['Content-Type' => 'application/json']
+  );
+
 });
 
 $app->run();
 
 
 
-/*
-
-docker - forawards to different containers.
-
-
-*/
